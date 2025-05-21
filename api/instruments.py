963fc9bd1f813a7d5448654422efd39710dc5e7b
Sneguradik <rrtr845@gmail.com>
2025-05-21 13:38:16 +0300

    Added class code filters

--- api/instruments.py
+++ api/instruments.py
@@ -6,7 +6,7 @@ from tinkoff.invest.async_services import AsyncServices
 
 async def get_instruments(ticker : str, client : AsyncServices) -> List[InstrumentShort] :
 
-    return list(filter(lambda x: True, (await client
+    return list(filter(lambda x: x.class_code in ["SPBXM", "FQBR", "TQBR"], (await client
        .instruments
        .find_instrument(query=ticker, instrument_kind= InstrumentType.INSTRUMENT_TYPE_SHARE))
        .instruments))   
